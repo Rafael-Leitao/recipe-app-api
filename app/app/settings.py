13a72677afb2e903f73adff40c6877d5d9a3be21
Rafael <rafael.s.leitao@gmail.com>
2024-12-09 09:37:04 -0800

    User API, tests and urls created

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-2
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'user',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
